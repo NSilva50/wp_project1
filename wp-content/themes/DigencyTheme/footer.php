0d87e2850b580e8ffe1bbbf33bcfc021dcc9fad4
User <nathansilva50@gmail.com>
2018-08-20 17:01:15 -0700

    5pm, practice due.

---
+++ wp-content/themes/DigencyTheme/footer.php
@@ -0,0 +1,26 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+				<div class="newsletter">
+					<h2 class="text-center">
+						Subscribe Newsletter
+					</h2>
+					<p class="text-center">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae repellendus fuga, ipsa facere maxime?
+					</p>
+					<div class="row justify-content-center">
+						<input type="text" placeholder="Enter your Email"> <button type="button" class="btn btn-dark">Submit</button>
+					</div>
+				</div>
+				<div class="bottomfooter row justify-content-between">
+					<a href="<?php echo home_url(); ?>">Digency</a>
+					<p>&copy; 2018 All Rights Reserved</p>
+				</div>
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
